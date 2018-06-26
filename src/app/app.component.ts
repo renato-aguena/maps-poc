e9f1da78dd872ba390574e5262ee4ccfb395337c
Renato Aguena <renato@MacBook-Pro-de-Renato.local>
2018-06-26 18:04:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'My first AGM project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  markers = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    let coords = {
+      lat: 51.678418,
+      lng: 7.809007
+    }
+
+    for (let _i = 0; _i < 5000; _i++) {
+      coords.lat += 0.000010;
+      coords.lng += 0.000010;
+      this.markers.push(Object.assign({}, coords))
+    }
+    console.log(this.markers)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    AgmSnazzyInfoWindowModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDBY6PGc4lS38m1yDH8u0uJRBaF1poH54M'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
